def98119d690af25647020111ffe1961fb2ed7e7
Andrew McKernan <amckerna@ualberta.ca>
2021-12-16 11:56:43 -0700

    added models

--- siteApp/models.py
+++ siteApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class MainService(models.Model):
+    name = models.CharField(max_length=200)
+    
+class Technology(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='media/images/')
+    
+class Subservice(models.Model):
+    name = models.CharField(max_length=200)
+    main_service = models.ForeignKey(MainService, on_delete=models.CASCADE)
+    technologies = models.ManyToManyField(Technology)
