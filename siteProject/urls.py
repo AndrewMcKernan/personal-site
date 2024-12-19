27654b3d9b2037a2c91d0c233d3dd49d3c7812cf
Andrew McKernan <dev@andrewmckernan.ca>
2024-12-18 18:03:28 -0700

    added testEnvironment app. Updated text on main site

--- siteProject/urls.py
+++ siteProject/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('siteApp.urls')),
+    path('testEnvironment/', include('testEnvironment.urls')),
 ]
