5ec7bf98f5de85da7a1195d95d9a105818d297f7
Andrew McKernan <andrew.mckernan@toptal.com>
2025-01-21 15:16:06 -0700

    increase size of token model fields

--- testEnvironment/models.py
+++ testEnvironment/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class CoreTokens(models.Model):
     _singleton = models.BooleanField(default=True, editable=False, unique=True)
     state = models.CharField(max_length=36, blank=True)
-    token_request_code = models.CharField(max_length=50, blank=True)  # I'm not sure how long this code needs to be
-    access_token = models.CharField(max_length=100, blank=True)
+    token_request_code = models.CharField(max_length=100, blank=True)  # I'm not sure how long this code needs to be
+    access_token = models.CharField(max_length=2000, blank=True)
     access_token_expiry = models.DateTimeField()
     endpoint = models.CharField(max_length=100, blank=True)
     access_token_type = models.CharField(max_length=10, blank=True)
-    refresh_token = models.CharField(max_length=100, blank=True)
+    refresh_token = models.CharField(max_length=2000, blank=True)
     refresh_token_expiry = models.DateTimeField()
