ddd8f31915baac70f3e1e1cf50125a0da6bcf0c2
Andrew McKernan <andrew.mckernan@toptal.com>
2025-02-25 23:39:39 -0700

    change link from tradepending to kelleybluebook

--- testEnvironment/core_api.py
+++ testEnvironment/core_api.py
@@ -45,8 +45,8 @@ def get_access_token(request):
     with transaction.atomic():
         core_token = CoreTokens.objects.select_for_update().get(_singleton=True)
         if code is None:
-            logger.error("Could not parse the authorization code from the callback, and therefore cannot get the "
-                         "access token.")
+            logger.error("Could not parse the authorization code from the callback, and therefore cannot request the "
+                         "access and refresh tokens.")
             core_token.waiting_for_tokens = False
             core_token.save()
             return
@@ -66,85 +66,11 @@ def get_access_token(request):
         core_token.access_token = json_data.get("access_token")
         core_token.access_token_expiry = datetime.datetime.now(timezone.utc) + \
                                          datetime.timedelta(seconds=json_data.get("expires_in"))
-        # core_token.refresh_token = json_data.get("refresh_token")
-        # core_token.refresh_token_expiry = datetime.datetime.now(timezone.utc) + \
-        #                                   datetime.timedelta(seconds=json_data.get("refresh_token_expires_in"))
+        core_token.refresh_token = json_data.get("refresh_token")
         core_token.endpoint = json_data.get("endpoint")
         core_token.access_token_type = json_data.get("token_type")
         core_token.save()
         logger.info("Tokens received.")
-        logger.info("Access token: " + str(core_token.access_token))
-        logger.info("Access token expiry: " + str(core_token.access_token_expiry))
-        logger.info("Endpoint: " + str(core_token.endpoint))
-        logger.info("Token Type: " + str(core_token.access_token_type))
-        # logger.info("Refresh token: " + str(core_token.refresh_token))
-        # logger.info("Refresh token expiry: " + str(core_token.refresh_token_expiry))
-    pass
-
-
-def get_refresh_code_uri():
-    logger = logging.getLogger(__name__)
-    base_uri = "https://api-identity.bqecore.com/idp/connect/authorize?"
-    core_token = None
-    with transaction.atomic():
-        if CoreTokens.objects.filter(_singleton=True).exists():
-            core_token = CoreTokens.objects.select_for_update().get(_singleton=True)
-        else:
-            logger.log("A refresh token was requested, but no core token was found.")
-            return
-        final_url = base_uri + urllib.parse.urlencode(
-            {"client_id": client_id, "scope": "offline_access", "redirect_uri":
-                redirect_uri, "response_type": response_type, "token": core_token.access_token})
-        logger.info("Requested an authentication code for a refresh token.")
-        # TODO: this is giving me an error indicating I have specified an invalid scope, despite the fact that the app
-        # is configured to allow offline access. I need to investigate this further.
-        return final_url
-
-
-def get_refresh_token(request):
-    # compare the state value from the request to the state value we set, and then set the code response
-    logger = logging.getLogger(__name__)
-    base_uri = "https://api-identity.bqecore.com/idp/connect/token"
-    code = request.GET.get('code', None)
-    logger.info(request.build_absolute_uri())
-    logger.info(request.body.decode('utf-8'))
-    with transaction.atomic():
-        core_token = CoreTokens.objects.select_for_update().get(_singleton=True)
-        if code is None:
-            logger.error("Could not parse the authorization code from the callback, and therefore cannot get the "
-                         "refresh and access tokens.")
-            core_token.waiting_for_tokens = False
-            core_token.save()
-            return
-        core_token.token_request_code = code
-        logger.info("Requesting refresh and access tokens.")
-        response = requests.post(base_uri, data={
-            "code": core_token.token_request_code,
-            "redirect_uri": redirect_uri,
-            "grant_type": "authorization_code",
-            "client_id": client_id,
-            "client_secret": "",
-        }, headers={"Content-Type": "application/x-www-form-urlencoded"})
-        logger.info(str(response.request.url))
-        logger.info(str(response))
-        json_data = response.json()
-        logger.info(str(json_data))
-        core_token.access_token = json_data.get("access_token")
-        core_token.access_token_expiry = datetime.datetime.now(timezone.utc) + \
-                                         datetime.timedelta(seconds=json_data.get("expires_in"))
-        core_token.refresh_token = json_data.get("refresh_token")
-        # core_token.refresh_token_expiry = datetime.datetime.now(timezone.utc) + \
-        #                                   datetime.timedelta(seconds=json_data.get("refresh_token_expires_in"))
-        # core_token.endpoint = json_data.get("endpoint")
-        core_token.access_token_type = json_data.get("token_type")
-        core_token.save()
-        logger.info("Tokens received.")
-        logger.info("Access token: " + str(core_token.access_token))
-        logger.info("Access token expiry: " + str(core_token.access_token_expiry))
-        # logger.info("Endpoint: " + str(core_token.endpoint))
-        logger.info("Token Type: " + str(core_token.access_token_type))
-        logger.info("Refresh token: " + str(core_token.refresh_token))
-        # logger.info("Refresh token expiry: " + str(core_token.refresh_token_expiry))
     pass
 
 
